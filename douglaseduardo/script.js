1e4f2b8ee432fc0cb820bf281f743686c6335f7e
DougFunny <douglaseduardo220483@gmail.com>
2019-09-30 19:42:48 -0300

    commmit final de noite

---
+++ douglaseduardo/script.js
@@ -0,0 +1,29 @@
+let fotoprincipal = document.querySelector("#fotoparatrocar")
+var foto1 = document.querySelector("#foto1 ")
+let foto2 = document.querySelector("#foto2")
+let foto3 = document.querySelector("#foto3")
+let foto4 = document.querySelector("#foto4")
+let foto_a = "imagens/desanimo.jpg"
+let foto_b = "imagens/mimimi.jpg"
+let foto_c = "imagens/pensando.jpg"
+let foto_d = "imagens/prantos.jpg"
+
+// trocarFotoObjeto = {
+//     1: function() { fotoprincipal.src = foto_a },
+//     2: function() { fotoprincipal.src = foto_b },
+//     3: function() { fotoprincipal.src = foto_c },
+//     4: function() { fotoprincipal.src = foto_d }
+// }
+
+// foto1.addEventListener("mouseover", function() {
+//     trocarFotoObjeto[1]
+// })
+// foto2.addEventListener("mouseover", function() {
+//     trocarFotoObjeto[2]
+// })
+// foto3.addEventListener("mouseover", function() {
+//     trocarFotoObjeto[3]
+// })
+// foto4.addEventListener("mouseover", function() {
+//     trocarFotoObjeto[4]
+// })
