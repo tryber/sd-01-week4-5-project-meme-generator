adc5bf9f6bce7b2cf791df8923447580d13f9538
antoniopaolillo <antonio_francisco_p@live.com>
2019-10-01 10:58:36 -0300

    add cores para as fontes, bordas e imagens precarregadas

--- antoniopaolillo/jsindex.js
+++ antoniopaolillo/jsindex.js
@@ -4,6 +4,7 @@ var finalbaixo = document.getElementById("textofinalbaixo"); //p final
 var txtcima = document.getElementById("textocima"); //txt de cima inicial
 var txtbaixo = document.getElementById("textobaixo"); //txt baixo inicial
 
+//colocando o texto na imagem
 txtcima.addEventListener('keyup', function(){
     finalcima.innerHTML = txtcima.value;
 })
@@ -12,8 +13,22 @@ txtbaixo.addEventListener('keyup', function(){
     finalbaixo.innerHTML = txtbaixo.value;
 })
 
+//adicionando a img ao div
 function loadFile(event) {
 	var image = document.getElementById('output');
     //caminho da imagem para o output
     image.src = URL.createObjectURL(event.target.files[0]);
 };
+
+//mudar a cor da fonte
+var cor = document.getElementById("color");
+cor.addEventListener('change', function(){
+    finalcima.style.color = cor.value;
+    finalbaixo.style.color = cor.value;
+})
+
+var display = document.getElementsByClassName("imgdisplay")[0];
+var borda = document.getElementById("borda");
+borda.addEventListener('change', function(){
+display.style.background = borda.value;
+})
