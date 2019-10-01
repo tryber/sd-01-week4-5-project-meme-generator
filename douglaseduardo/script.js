5a30efab985451cd9dab9a11d13d9babfc745ad9
dougfunny1983 <douglaseduardo220483@gmail.com>
2019-10-01 15:39:59 -0300

    recolocando os arquivos modificados

--- douglaseduardo/script.js
+++ douglaseduardo/script.js
@@ -1,29 +1,62 @@
+//variaveis foto
 let fotoprincipal = document.querySelector("#fotoparatrocar")
-var foto1 = document.querySelector("#foto1 ")
+let foto1 = document.querySelector("#foto1")
 let foto2 = document.querySelector("#foto2")
 let foto3 = document.querySelector("#foto3")
 let foto4 = document.querySelector("#foto4")
+let foto5 = document.querySelector("#foto5")
+//variaveis texto
+let textoDoForms = document.querySelector(".texto")
+let textoDaTagP = document.querySelector("#texto_meme")
+//variaveis dos arquivos
 let foto_a = "imagens/desanimo.jpg"
 let foto_b = "imagens/mimimi.jpg"
 let foto_c = "imagens/pensando.jpg"
 let foto_d = "imagens/prantos.jpg"
+let foto_e = "imagens/engomadinho.jpg"
+//funções de colocar o texto
+function colocarTexto() {
+    textoDaTagP.innerHTML = textoDoForms.innerHTML
+}
+//funções de trocar as fotos
+function trocarFoto(parametro) {
+    fotoprincipal.src = parametro 
+}
+//funções dos eventos
+// seção de mouser ouver
+foto1.addEventListener("mouseover", function() {
+    trocarFoto(foto_a)
+})
+foto2.addEventListener("mouseover", function() {
+    trocarFoto(foto_b)
+})
+foto3.addEventListener("mouseover", function() {
+    trocarFoto(foto_c)
+})
+foto4.addEventListener("mouseover", function() {
+    trocarFoto(foto_d)
+})
+foto5.addEventListener("mouseover", function() {
+    trocarFoto(foto_e)
+})
+// seção de onclick
+foto1.addEventListener("click", function(){
+    trocarFoto(foto_a)
+})
+foto2.addEventListener("click", function(){
+    trocarFoto(foto_b)
+})
+foto3.addEventListener("click", function(){
+    trocarFoto(foto_c)
+})
+foto4.addEventListener("click", function(){
+    trocarFoto(foto_d)
+})
+foto5.addEventListener("click", function(){
+    trocarFoto(foto_e)
+})
+//seção para o texto
+textoDoForms.addEventListener("keyup", function{
+    colocarTexto()
+})
 
-// trocarFotoObjeto = {
-//     1: function() { fotoprincipal.src = foto_a },
-//     2: function() { fotoprincipal.src = foto_b },
-//     3: function() { fotoprincipal.src = foto_c },
-//     4: function() { fotoprincipal.src = foto_d }
-// }
-
-// foto1.addEventListener("mouseover", function() {
-//     trocarFotoObjeto[1]
-// })
-// foto2.addEventListener("mouseover", function() {
-//     trocarFotoObjeto[2]
-// })
-// foto3.addEventListener("mouseover", function() {
-//     trocarFotoObjeto[3]
-// })
-// foto4.addEventListener("mouseover", function() {
-//     trocarFotoObjeto[4]
-// })
