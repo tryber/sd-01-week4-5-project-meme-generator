131fba65b2e4b1064ec383cafb40007ed2f1b672
Anderson Bolivar <bolivar.anderson@hotmail.com>
2019-10-02 18:10:05 -0300

    Select

--- bolivar/meme-generator.js
+++ bolivar/meme-generator.js
@@ -1,10 +1,29 @@
 // Upload Image
 // Event itself contains the properties and methods which are common to all events
-function handleFiles(event) {
-    var img = document.getElementById("meme-image");
+function handleFile1(event) {
+    var img = document.getElementById("meme-image1");
     // Return a DOMString containing an object URL
     // The target property of the Event interface is a reference to the object that dispatched the event
     img.src = URL.createObjectURL(event.target.files[0]);
 }; 
+
+function handleFile2(event) {
+    var img = document.getElementById("meme-image2");
+    // Return a DOMString containing an object URL
+    // The target property of the Event interface is a reference to the object that dispatched the event
+    img.src = URL.createObjectURL(event.target.files[0]);
+}; 
+
+    
+function select(x) {
+    if (x.selectedIndex === 0){
+        document.getElementsByClassName("meme-R")[0].style.display = "none";
+    } 
+    else if (x.selectedIndex === 1){
+        document.getElementsByClassName("meme-R")[0].style.display = "block";
+    }
+}; 
+
+    
 
 
