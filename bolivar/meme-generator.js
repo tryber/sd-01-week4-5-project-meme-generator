159623f53c38c23cc57498385c577ce0bf9bb837
Anderson Bolivar <bolivar.anderson@hotmail.com>
2019-10-03 10:49:19 -0300

    Project 1/3 Done

--- bolivar/meme-generator.js
+++ bolivar/meme-generator.js
@@ -1,3 +1,17 @@
+// Select Image
+function select(x) {
+    if (x.selectedIndex === 0){
+        for(i = 0; i < document.getElementsByClassName("meme-R").length; i++){
+            document.getElementsByClassName("meme-R")[i].style.display = "none";
+        } 
+    } 
+    else if (x.selectedIndex === 1){
+        for(i = 0; i < document.getElementsByClassName("meme-R").length; i++){
+        document.getElementsByClassName("meme-R")[i].style.display = "block";
+        }
+    }
+};
+
 // Upload Image
 // Event itself contains the properties and methods which are common to all events
 function handleFile1(event) {
@@ -9,21 +23,25 @@ function handleFile1(event) {
 
 function handleFile2(event) {
     var img = document.getElementById("meme-image2");
-    // Return a DOMString containing an object URL
-    // The target property of the Event interface is a reference to the object that dispatched the event
     img.src = URL.createObjectURL(event.target.files[0]);
 }; 
 
-    
-function select(x) {
-    if (x.selectedIndex === 0){
-        document.getElementsByClassName("meme-R")[0].style.display = "none";
-    } 
-    else if (x.selectedIndex === 1){
-        document.getElementsByClassName("meme-R")[0].style.display = "block";
-    }
-}; 
+// Select the text    
+function leftTextTop(x){
+    document.getElementById("text-left-top").innerHTML = x.value;
+}
 
-    
+function leftTextBottom(x){
+    document.getElementById("text-left-bottom").innerHTML = x.value;
+}
+
+function rightTextTop(x){
+    document.getElementById("text-right-top").innerHTML = x.value;
+}
+
+function rightTextBottom(x){
+    document.getElementById("text-right-bottom").innerHTML = x.value;
+}
+
 
 
