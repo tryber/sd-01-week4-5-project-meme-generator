c54a1920a38bc370482bd71fd4a941b264cf09d3
Henrique Eyer <henriqueeyer@outlook.com>
2019-10-07 17:41:39 -0300

    Adicionando um for para adicionar event de click nos memes já prontos

--- Henrique-Eyer/javascript.js
+++ Henrique-Eyer/javascript.js
@@ -26,86 +26,93 @@ var qtd = 0;
 
 //função para salvar os memes já criados
 salvar.addEventListener('click', function(){
-
-  var pegamemepS = document.createElement("p");
-  pegamemepS.className = "txt-meme c";
-  pegamemepS.id = "txt-memec"+qtd;
-  pegamemepS.innerHTML = inputtextoc.value
-  pegamemepS.style.fontSize = txtUP.value*0.25;
-  pegamemepS.style.color = corSup.value;
-
-  var pegamemepI = document.createElement("p");
-  pegamemepI.className = "txt-meme b";
-  pegamemepI.id = "txt-memeb"+qtd;
-  pegamemepI.innerHTML = inputtextob.value;
-  pegamemepI.style.color = corInf.value;
-  pegamemepI.style.fontSize = txtDown*0.25;
-
-  var pegamemeimg = document.createElement("img");
-  pegamemeimg.src = divImg.src;
-
-  var pegameme = document.createElement("div");
-  pegameme.className = "itemcolection";
-
-  
-  
-  
-  var divcollection = document.querySelector(".collection");
-  divcollection.appendChild(pegameme);
-  pegameme.appendChild(pegamemeimg);
-  pegameme.appendChild(pegamemepI);
-  pegameme.appendChild(pegamemepS);
-  qtd++;
+    
+    var pegamemepS = document.createElement("p");
+    pegamemepS.className = "txt-meme c";
+    pegamemepS.id = "txt-memec"+qtd;
+    pegamemepS.innerHTML = inputtextoc.value
+    pegamemepS.style.fontSize = txtUP.value*0.25;
+    pegamemepS.style.color = corSup.value;
+    
+    var pegamemepI = document.createElement("p");
+    pegamemepI.className = "txt-meme b";
+    pegamemepI.id = "txt-memeb"+qtd;
+    pegamemepI.innerHTML = inputtextob.value;
+    pegamemepI.style.color = corInf.value;
+    pegamemepI.style.fontSize = txtDown*0.25;
+    
+    var pegamemeimg = document.createElement("img");
+    pegamemeimg.src = divImg.src;
+    
+    var pegameme = document.createElement("div");
+    pegameme.className = "itemcolection";
+    
+    
+    
+    
+    var divcollection = document.querySelector(".collection");
+    divcollection.appendChild(pegameme);
+    pegameme.appendChild(pegamemeimg);
+    pegameme.appendChild(pegamemepI);
+    pegameme.appendChild(pegamemepS);
+    qtd++;
 })
 
 
 //função para mudar a bordar
 borda.addEventListener('change', function(){
-  bordadiv.style.borderStyle = borda.value;
+    bordadiv.style.borderStyle = borda.value;
 })
 
-// Funçoes para mudar as imagem caso clique 
-colecao[0].addEventListener('click', function (){
-  divImg.src = colecao[0].src;
-})
-colecao[1].addEventListener('click', function (){
-  divImg.src = colecao[1].src;
-})
-colecao[2].addEventListener('click', function (){
-  divImg.src = colecao[2].src;
-})
-colecao[3].addEventListener('click', function (){
-  divImg.src = colecao[3].src;
-})
+
+// Funçoes para mudar as imagem caso clique
+for(let i=0;i<colecao.length;i++){
+    colecao[i].addEventListener('click', function (){
+        divImg.src = colecao[i].src;
+    })
+}
+
+// colecao[0].addEventListener('click', function (){
+//   divImg.src = colecao[0].src;
+// })
+// colecao[1].addEventListener('click', function (){
+//   divImg.src = colecao[1].src;
+// })
+// colecao[2].addEventListener('click', function (){
+//   divImg.src = colecao[2].src;
+// })
+// colecao[3].addEventListener('click', function (){
+//   divImg.src = colecao[3].src;
+// })
 
 
 //funçoes para mudar o valor do font size dos textos da imagem
 txtUP.addEventListener('change', function (){
-  if(txtUP.value > 55){
-    txtUP.value = 55;
-  }
-  if(txtUP.value < 15){
-    txtUP.value = 15;
-  }
-  ptxtc.style.fontSize = txtUP.value;
+    if(txtUP.value > 55){
+        txtUP.value = 55;
+    }
+    if(txtUP.value < 15){
+        txtUP.value = 15;
+    }
+    ptxtc.style.fontSize = txtUP.value;
 })
 txtDown.addEventListener('change', function (){
-  if(txtDown.value > 55){
-    txtDown.value = 55;
-  }
-  if(txtDown.value < 15){
-    txtDown.value = 15;
-  }
-  ptxtb.style.fontSize = txtDown.value;
+    if(txtDown.value > 55){
+        txtDown.value = 55;
+    }
+    if(txtDown.value < 15){
+        txtDown.value = 15;
+    }
+    ptxtb.style.fontSize = txtDown.value;
 })
 //fim funçoes para mudar o tamanho
 
 //função para modificar as cores
 corSup.addEventListener('change', function(){
-  ptxtc.style.color = corSup.value;
+    ptxtc.style.color = corSup.value;
 })
 corInf.addEventListener('change', function(){
-  ptxtb.style.color = corInf.value;
+    ptxtb.style.color = corInf.value;
 })
 
 
@@ -113,27 +120,27 @@ corInf.addEventListener('change', function(){
 
 //função para ativar o load file quando o usuario escolher a imagem
 img.addEventListener('change', function (){
-  
-  loadFile(event);
-  
+    
+    loadFile(event);
+    
 })
 
 //função para adicionar o evento de keyup para parte de cima do texto 
 //função faz com que o paragrafo de dentro da imgem receba o texto digitado
 inputtextoc.addEventListener('keyup', function (){
-  
-  ptxtc.innerHTML = inputtextoc.value;
-  
+    
+    ptxtc.innerHTML = inputtextoc.value;
+    
 })
 //função para adicionar o evento de keyup para parte de baixo do texto
 //função faz com que o paragrafo de dentro da imgem receba o texto digitado
 inputtextob.addEventListener('keyup', function (){
-  
-  ptxtb.innerHTML = inputtextob.value;
-  
+    
+    ptxtb.innerHTML = inputtextob.value;
+    
 })
 
 //variavel que recebe a função para carregar a imagem na div.
 var loadFile = function(event) {
-  divImg.src = URL.createObjectURL(event.target.files[0]);
+    divImg.src = URL.createObjectURL(event.target.files[0]);
 };
