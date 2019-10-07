85c755f68cfa1bc96e495c113af12988c737ef5a
Guilherme Luiz <55408463+guiiluiz@users.noreply.github.com>
2019-10-07 18:22:25 -0300

    transforma funcoes duplicadas em uma unica
    transforma funcoes parecidas TextoCima e TextoBaixo em uma unica TextoFrase

--- guiiluiz/script.js
+++ guiiluiz/script.js
@@ -1,17 +1,11 @@
-
 function loadFile(event) {
     var image = document.getElementById('output');
     image.src = URL.createObjectURL(event.target.files[0]);
 };
 
-function textoCima(frasecima) {
-    var txtcima = document.getElementById("textocima")
-    txtcima.innerHTML = frasecima.value;
-}
-
-function textoBaixo(frasebaixo) {
-    var txtbaixo = document.getElementById("textobaixo")
-    txtbaixo.innerHTML = frasebaixo.value;
+function textoFrase(frase,id) {
+    var txtbaixo = document.getElementById(id)
+    txtbaixo.innerHTML = frase.value;
 }
 
 function aumentaLetra() {
