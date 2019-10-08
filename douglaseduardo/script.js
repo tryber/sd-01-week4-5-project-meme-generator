d1893c9525af4b8044d172fb0c87f8a59d49f959
DougFunny <douglaseduardo220483@gmail.com>
2019-10-08 19:11:32 -0300

    Terminado as auteraçoes que o alberto solicitou

--- douglaseduardo/script.js
+++ douglaseduardo/script.js
@@ -17,7 +17,7 @@ let textoMeme = document.querySelector(".textoMeme")
 let fotoA = "imagens/godplease.jpg"
 let fotoB = "imagens/jura.jpg"
 let fotoC = "imagens/kiko.jpg"
-let fotoC = "imagens/mudarseassunto.jpg"
+let fotoD = "imagens/mudarseassunto.jpg"
 let fotoE = "imagens/rir.jpg"
 let fotoUpload = document.querySelector("#imagemCarregada")
 let btnSubmit = document.querySelector("#fotoUpload")
@@ -27,39 +27,36 @@ let corFonte2 = document.querySelector("#coloridoFONTE2")
 
 //funções de trocar as fotos
 function trocarFoto(parametro) {
-    fotoprincipal.src = parametro 
+    fotoPrincipal.src = parametro 
 }
 
 //adicionando a img ao div
 function loadFile(event) {
-    fotoprincipal.src = URL.createObjectURL(event.target.files[0]);
+    fotoPrincipal.src = URL.createObjectURL(event.target.files[0]);
 }
 // seção de onclick
 foto1.addEventListener("click", function(){
-    trocarFoto(foto_a)
+    trocarFoto(fotoA)
 })
 foto2.addEventListener("click", function(){
-    trocarFoto(foto_b)
+    trocarFoto(fotoB)
 })
 foto3.addEventListener("click", function(){
-    trocarFoto(foto_c)
+    trocarFoto(fotoC)
 })
 foto4.addEventListener("click", function(){
-    trocarFoto(foto_d)
+    trocarFoto(fotoD)
 })
 foto5.addEventListener("click", function(){
-    trocarFoto(foto_e)
+    trocarFoto(fotoE)
 })
 
-//seção para o texto
 texto1.addEventListener("keyup", function(){
-textocima.innerHTML = texto1.value
+textoCima.innerHTML = texto1.value
 })
 texto2.addEventListener("keyup", function(){
-textobaixo.innerHTML = texto2.value
+textoBaixo.innerHTML = texto2.value
 })
-
-//função para salvar o documento
 
 function printDiv(divName) {
     var printContents = document.getElementById(divName).innerHTML;
@@ -68,14 +65,15 @@ function printDiv(divName) {
     window.print();
     document.body.innerHTML = originalContents;
 }
-//função trocar cor moldura
+
 corBorda.addEventListener("change", function(){
     divPrincipal.style.borderColor = corBorda.value
 })
-//cor da fonte
+
 corFonte1.addEventListener("change", function(){
     textocima.style.color = corFonte1.value
 })
+
 corFonte2.addEventListener("change", function(){
     textobaixo.style.color = corFonte2.value
 })
