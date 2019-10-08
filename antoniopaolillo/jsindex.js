ffcb9f33cd02771187ffb56315d5d2f981885f8a
antoniopaolillo <antonio_francisco_p@live.com>
2019-10-08 10:08:57 -0300

    realizando mudanças do codereview

--- antoniopaolillo/jsindex.js
+++ antoniopaolillo/jsindex.js
@@ -1,67 +1,52 @@
-var finalcima = document.getElementById("textofinalcima"); //p final
-var finalbaixo = document.getElementById("textofinalbaixo"); //p final
-var txtcima = document.getElementById("textocima"); //txt de cima inicial
-var txtbaixo = document.getElementById("textobaixo"); //txt baixo inicial
+var textoSuperiorFinal = document.getElementById("textofinalcima"); //p final
+var textoInferiorFinal = document.getElementById("textofinalbaixo"); //p final
+var textoSuperior = document.getElementById("textocima"); //txt de cima inicial
+var textoInferior = document.getElementById("textobaixo"); //txt baixo inicial
 
 //colocando o texto na imagem
-txtcima.addEventListener('keyup', function(){
-    finalcima.innerHTML = txtcima.value;
+textoSuperior.addEventListener('keyup', function () {
+    textoSuperiorFinal.innerHTML = textoSuperior.value;
 })
 
-txtbaixo.addEventListener('keyup', function(){
-    finalbaixo.innerHTML = txtbaixo.value;
+textoInferior.addEventListener('keyup', function () {
+    textoInferiorFinal.innerHTML = textoInferior.value;
 })
 
 //adicionando a img ao div
-function loadFile(event) {
-	var image = document.getElementById('output');
+function adicionandoImagem(event) {
+    var image = document.getElementById('output');
     //caminho da imagem para o output
     image.src = URL.createObjectURL(event.target.files[0]);
 };
 
 //mudar a cor da fonte
 var cor = document.getElementById("color");
-cor.addEventListener('change', function(){
-    finalcima.style.color = cor.value;
-    finalbaixo.style.color = cor.value;
+cor.addEventListener('change', function () {
+    textoSuperiorFinal.style.color = cor.value;
+    textoInferiorFinal.style.color = cor.value;
 })
 
 //alterando a borda
 var display = document.getElementById('output');
 var borda = document.getElementById("borda");
-borda.addEventListener('change', function(){
-display.style.borderWidth = borda.value;
+borda.addEventListener('change', function () {
+    display.style.borderWidth = borda.value;
 })
 
 //buscando imagens precarregadas e mostrando na tela
 var imgprincipal = document.getElementById('output');
-var im1 = document.getElementById('img1');
-var im2 = document.getElementById('img2');
-var im3 = document.getElementById('img3');
-var im4 = document.getElementById('img4');
-var im5 = document.getElementById('img5');
-
-im1.addEventListener('click', function(){
-    imgprincipal.src = im1.src;
-})
-im2.addEventListener('click', function(){
-    imgprincipal.src = im2.src;
-})
-im3.addEventListener('click', function(){
-    imgprincipal.src = im3.src;
-})
-im4.addEventListener('click', function(){
-    imgprincipal.src = im4.src;
-})
-im5.addEventListener('click', function(){
-    imgprincipal.src = im5.src;
-})
+for (let i = 0; i < 5; i++) {
+    let imagemCarregada = document.getElementsByClassName("imgcarregadas")[i];
+    imagemCarregada.addEventListener('click', function () {
+        imgprincipal.src = imagemCarregada.src;
+    })
+}
 
 //alterando o tamanho da fonte 
 var tam = document.getElementById("tamanho");
-tam.addEventListener('change', function(){
-    finalcima.style.fontSize = tam.value;
-    finalbaixo.style.fontSize = tam.value;
+tam.addEventListener('change', function () {
+    textoSuperiorFinal.style.fontSize = tam.value;
+    textoInferiorFinal.style.fontSize = tam.value;
 })
 
 //baixando a imagem
