1590c310f8205c3b5cec0ffb61124ff4faadeafd
Conrado Medeiros <conradodemedeiros@hotmail.com>
2019-10-09 14:40:13 -0300

    button plus

--- conrado/script.js
+++ conrado/script.js
@@ -23,8 +23,19 @@ function showTextBottom(event) {
    let showText2 = document.getElementById('textbottom');
    showText2.innerHTML = text2;
 }  
-
 var buttonplus = document.getElementById("btnmaistext1")
-    buttonplus.addEventListener("click", function teste() {
-    let plus = document.getElementById("texttop").style.fontSize = "large";
+buttonplus.addEventListener("click", function addFontSize() {
+    let plus = document.getElementById("texttop").style.fontSize = "50px";
+})
+var buttonless = document.getElementById("btnmenostext1")
+buttonless.addEventListener("click", function lessFontSize() {
+    let less = document.getElementById("texttop").style.fontSize = "20px";
+})
+var buttonplus2 = document.getElementById("btnmaistext2")
+buttonplus2.addEventListener("click", function addFontSize() {
+    let plus2 = document.getElementById("textbottom").style.fontSize = "50px";
+})
+var buttonless2 = document.getElementById("btnmenostext2")
+buttonless2.addEventListener("click", function lessFontSize() {
+    let less2 = document.getElementById("textbottom").style.fontSize = "20px";
 })
