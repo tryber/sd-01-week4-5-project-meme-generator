bff6aee81bb2d8989c51e423d15da1a7da777561
Victor Felipe <vf.ramos7@gmail.com>
2019-10-09 18:43:52 -0300

    retirado a chamada da funcao de inserir imagem via HTML e adicionado uma chamada via JavaScript atraves do addEventListener

--- vrramos/js/script.js
+++ vrramos/js/script.js
@@ -1,3 +1,11 @@
+function inserirImagem() {
+    let imagemInput = document.querySelector('#carregar-imagem')
+
+    imagemInput.addEventListener('change', function(){
+        carregarImagem(event)
+    })
+}
+
 function carregarImagem(event) {
     let img = document.getElementsByClassName('imagem-inserida')[0];
     img.src = URL.createObjectURL(event.target.files[0]);
@@ -8,3 +16,5 @@ function writeText(){
     let textoOutput = document.querySelector('#valor-saida')
     textoOutput.textContent = textoInput;
 }   
+
+inserirImagem()
